420e4b4de26f99e8768b4db4f9384eab2cb67461
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-29 15:21:22 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-sm">hello</h1>
+      <Header></Header>
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../../public/images/Tasty_HUT.png';
+
+const Header = () => {
+    return (
+        <div className="w-full bg-gray-300 py-3 px-16 flex justify-between items-center">
+            <img className="h-6" src={logo} alt="" />
+            <div className='flex items-center'>
+                <input className="h-8 w-[300px] rounded-l text-sm pl-3 outline-none font-semibold" type="text" placeholder="Search food name"/>
+                <button className="px-3 h-8 bg-lime-700 font-semibold text-base text-white rounded outline-none">Search</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
