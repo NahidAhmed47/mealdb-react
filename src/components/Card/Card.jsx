30e62297213ffc1ad4e153778df9bead6b4a978f
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-29 15:50:56 +0600

    data load and show card done

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = ({cart}) => {
+    console.log(cart);
+    const {strMealThumb, strMeal, strArea, strCategory} = cart;
+    return (
+        <>
+            <div className="p-1 border-2 rounded-md">
+                <img className="rounded" src={strMealThumb} alt="" />
+                <div className="m-2">
+                    <h1 className="font-semibold text-base">Name: {strMeal}</h1>
+                    <p className="text-sm font-semibold my-1">Country origin: {strArea}</p>
+                    <p className="text-sm font-semibold">Category: {strCategory}</p>
+                </div>
+                    <button className="font-semibold text-base h-8 w-full bg-lime-700 text-white rounded">Pick Items</button>
+            </div>
+        </>
+    );
+};
+
+export default Card;
---
+++ src/components/DisplayMeal/DisplayMeal.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+
+const DisplayMeal = () => {
+    const [cart, setCart] = useState([]);
+    useEffect(()=>{
+        const LoadData = async()=>{
+            const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=c`);
+            const data = await res.json();
+            setCart(data.meals)
+        }
+        LoadData()
+    },[])
+    return (
+        <div className="grid grid-cols-5 min-h-[100vh]">
+            <div className="col-span-4 w-full">
+                <h1 className="font-semibold text-base text-center mt-4">Your Searched food({cart.length}) show here:</h1>
+                <div className="w-full px-10 pt-5 grid grid-cols-4 gap-5">
+                    {
+                        cart.map(singleCart => <Card cart={singleCart} key={singleCart.id}></Card>)
+                    }
+                </div>
+            </div>
+            <div className="bg-lime-300 w-full">
+                <h1 className="font-semibold text-base text-center mt-4">Added Items:</h1>
+            </div>
+        </div>
+    );
+};
+
+export default DisplayMeal;
